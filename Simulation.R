229fae75fc87bb91890f9e47d99072edb54f628c
Huike Cheng <huikecheng@outlook.com>
2023-05-30 12:50:28 +0100

    update

--- Simulation.R
+++ Simulation.R
@@ -2,6 +2,7 @@
 library(rpart)
 library(sharp)
 library(fake)
+source("stab2.R")
 
 ######### simulate
 set.seed(1)
@@ -11,37 +12,81 @@ simul <- SimulateRegression(pk = 50)
 myvars_T <- rownames(simul$beta)[which(simul$beta != 0)]
 myvars_F <- rownames(simul$beta)[which(simul$beta == 0)]
 
-######### run stab
-stab <- sharp::VariableSelection(
-  xdata = simul$xdata, ydata = simul$ydata,
-  implementation = CART,
-  family = "gaussian",
-)
-myvars_stab <- names(SelectedVariables(stab))[which(SelectedVariables(stab)!=0)]
-myscore_stab <- SelectionProportions(stab)
-
 ######## run CART
 mydata <- data.frame(ydata = simul$ydata[, 1], simul$xdata)
 mycart <- rpart(ydata ~ ., mydata, method="anova", )
 myvars_cart <- names(mycart$variable.importance)
 myscore_cart <- mycart$variable.importance
 
-####### Confusion matrix
-TP_stab <- sum(myvars_T %in% myvars_stab)
-FP_stab <- sum(myvars_F %in% myvars_stab)
-TN_stab <- sum(!(myvars_F %in% myvars_stab))
-FN_stab <- sum(!(myvars_T %in% myvars_stab))
+######### run stab
+stab <- sharp::VariableSelection(
+  xdata = simul$xdata, ydata = simul$ydata,
+  implementation = CART,
+  family = "gaussian"
+)
+myvars_stab <- names(SelectedVariables(stab))[which(SelectedVariables(stab)!=0)]
+myscore_stab <- SelectionProportions(stab)
 
+####### run stab2
+Lambda <- mycart$cptable[,1]
+stab2 <- sharp::VariableSelection(
+  xdata = simul$xdata, ydata = simul$ydata,
+  implementation = CART2,
+  family = "gaussian",
+  Lambda = Lambda
+)
+myvars_stab2 <- names(SelectedVariables(stab2))[which(SelectedVariables(stab2)!=0)]
+myscore_stab2 <- SelectionProportions(stab2)
+
+####### Confusion matrix
 TP_cart <- sum(myvars_T %in% myvars_cart)
 FP_cart <- sum(myvars_F %in% myvars_cart)
 TN_cart <- sum(!(myvars_F %in% myvars_cart))
 FN_cart <- sum(!(myvars_T %in% myvars_cart))
 
+TP_stab <- sum(myvars_T %in% myvars_stab)
+FP_stab <- sum(myvars_F %in% myvars_stab)
+TN_stab <- sum(!(myvars_F %in% myvars_stab))
+FN_stab <- sum(!(myvars_T %in% myvars_stab))
+
+TP_stab2 <- sum(myvars_T %in% myvars_stab2)
+FP_stab2 <- sum(myvars_F %in% myvars_stab2)
+TN_stab2 <- sum(!(myvars_F %in% myvars_stab2))
+FN_stab2 <- sum(!(myvars_T %in% myvars_stab2))
+
 ########## metrics
+sensitivity_cart <- TP_cart/length(myvars_T)
+precision_cart <- TP_cart/length(myvars_cart)
+f1_cart <- 2*sensitivity_cart*precision_cart/(sensitivity_cart+precision_cart)
+
 sensitivity_stab <- TP_stab/length(myvars_T)
 precision_stab <- TP_stab/length(myvars_stab)
 f1_stab <- 2*sensitivity_stab*precision_stab/(sensitivity_stab+precision_stab)
 
-sensitivity_cart <- TP_cart/length(myvars_T)
-precision_cart <- TP_cart/length(myvars_cart)
-f1_cart <- 2*sensitivity_cart*precision_cart/(sensitivity_cart+precision_cart)
+sensitivity_stab2 <- TP_stab2/length(myvars_T)
+precision_stab2 <- TP_stab2/length(myvars_stab2)
+f1_stab2 <- 2*sensitivity_stab2*precision_stab2/(sensitivity_stab2+precision_stab2)
+
+########### S vs F1
+F1 <- matrix(0, nrow=nrow(stab2$S_2d), ncol=ncol(stab2$S_2d))
+for (i in 1:nrow(stab2$S_2d)) {
+  for (j in 1:ncol(stab2$S_2d)) {
+  myvars_stab2 <- Stable(stab2, argmax_id = c(i,j))
+  myvars_stab2 <- names(myvars_stab2)[myvars_stab2 == 1]
+  TP_stab2 <- sum(myvars_T %in% myvars_stab2)
+  FP_stab2 <- sum(myvars_F %in% myvars_stab2)
+  TN_stab2 <- sum(!(myvars_F %in% myvars_stab2))
+  FN_stab2 <- sum(!(myvars_T %in% myvars_stab2))
+  sensitivity_stab2 <- TP_stab2/length(myvars_T)
+  precision_stab2 <- TP_stab2/length(myvars_stab2)
+  f1_stab2 <- 2*sensitivity_stab2*precision_stab2/(sensitivity_stab2+precision_stab2)
+  F1[i,j] <- f1_stab2
+  }
+}
+
+F1 <- c(F1)
+S <- c(stab2$S_2d)
+df <- data.frame(S = S, F1 = F1)
+df <- df[!is.nan(df$S),]
+
+ggplot(df, aes(x=F1, y=S)) + geom_point()
--- Simulation_Linear.R
+++ Simulation_Linear.R
@@ -4,6 +4,7 @@ library(sharp)
 library(fake)
 library(rlist)
 library(ggplot2)
+source("stab2.R")
 
 ######### function
 Simulation_study <- function(seed, pk) {
@@ -14,6 +15,13 @@ Simulation_study <- function(seed, pk) {
   # vars
   myvars_T <- rownames(simul$beta)[which(simul$beta != 0)]
   myvars_F <- rownames(simul$beta)[which(simul$beta == 0)]
+  
+  ######## run CART
+  mydata <- data.frame(ydata = simul$ydata[, 1], simul$xdata)
+  mycart <- rpart(ydata ~ ., mydata, method="anova", )
+  myvars_cart <- names(mycart$variable.importance)
+  myscore_cart <- mycart$variable.importance
+  
   ######### run stab
   stab <- sharp::VariableSelection(
     xdata = simul$xdata, ydata = simul$ydata,
@@ -22,40 +30,53 @@ Simulation_study <- function(seed, pk) {
   )
   myvars_stab <- names(SelectedVariables(stab))[which(SelectedVariables(stab)!=0)]
   myscore_stab <- SelectionProportions(stab)
-  ######## run CART
-  mydata <- data.frame(ydata = simul$ydata[, 1], simul$xdata)
-  mycart <- rpart(ydata ~ ., mydata, method="anova", )
-  myvars_cart <- names(mycart$variable.importance)
-  myscore_cart <- mycart$variable.importance
-  ####### Confusion matrix
-  TP_stab <- sum(myvars_T %in% myvars_stab)
-  FP_stab <- sum(myvars_F %in% myvars_stab)
-  TN_stab <- sum(!(myvars_F %in% myvars_stab))
-  FN_stab <- sum(!(myvars_T %in% myvars_stab))
+
+  ####### run stab2
+  Lambda <- mycart$cptable[,1]
+  stab2 <- sharp::VariableSelection(
+    xdata = simul$xdata, ydata = simul$ydata,
+    implementation = CART2,
+    family = "gaussian",
+  )
+  myvars_stab2 <- names(SelectedVariables(stab2))[which(SelectedVariables(stab2)!=0)]
+  myscore_stab2 <- SelectionProportions(stab2)
   
+  ####### Confusion matrix
   TP_cart <- sum(myvars_T %in% myvars_cart)
   FP_cart <- sum(myvars_F %in% myvars_cart)
   TN_cart <- sum(!(myvars_F %in% myvars_cart))
   FN_cart <- sum(!(myvars_T %in% myvars_cart))
   
-  ########## metrics
-  sensitivity_stab <- TP_stab/length(myvars_T)
-  precision_stab <- TP_stab/length(myvars_stab)
-  f1_stab <- 2*sensitivity_stab*precision_stab/(sensitivity_stab+precision_stab)
-  metrics_stab <- c(sensitivity_stab, precision_stab, f1_stab)
-  names(metrics_stab) <- c("Sensitivity", "Precision", "F1")
+  TP_stab <- sum(myvars_T %in% myvars_stab)
+  FP_stab <- sum(myvars_F %in% myvars_stab)
+  TN_stab <- sum(!(myvars_F %in% myvars_stab))
+  FN_stab <- sum(!(myvars_T %in% myvars_stab))
   
+  TP_stab2 <- sum(myvars_T %in% myvars_stab2)
+  FP_stab2 <- sum(myvars_F %in% myvars_stab2)
+  TN_stab2 <- sum(!(myvars_F %in% myvars_stab2))
+  FN_stab2 <- sum(!(myvars_T %in% myvars_stab2))
+  
+  ########## metrics
   sensitivity_cart <- TP_cart/length(myvars_T)
   precision_cart <- TP_cart/length(myvars_cart)
   f1_cart <- 2*sensitivity_cart*precision_cart/(sensitivity_cart+precision_cart)
-  metrics_cart <- c(sensitivity_cart, precision_cart, f1_cart)
-  names(metrics_cart) <- c("Sensitivity", "Precision", "F1")
+  
+  sensitivity_stab <- TP_stab/length(myvars_T)
+  precision_stab <- TP_stab/length(myvars_stab)
+  f1_stab <- 2*sensitivity_stab*precision_stab/(sensitivity_stab+precision_stab)
+  
+  sensitivity_stab2 <- TP_stab2/length(myvars_T)
+  precision_stab2 <- TP_stab2/length(myvars_stab2)
+  f1_stab2 <- 2*sensitivity_stab2*precision_stab2/(sensitivity_stab2+precision_stab2)
   
   ##### output
   return(list(cart = list(Metrics = metrics_cart,
                         Scores = myscore_cart),
               stab = list(Metrics = metrics_stab,
-                          Scores = myscore_stab)))
+                          Scores = myscore_stab),
+              stab2 = list(Metrics = metrics_stab2,
+                           Scores = myscore_stab2)))
 }
 
 a <- Simulation_study(1,50)
